251320009c5f4e897170da7f72fe4b9a16b932cc
Gunnar Pruefer <gunnar.pruefer@gmx.de>
2025-08-31 22:33:42 +0200

    add table

--- src/main/java/org/acme/UserResource.java
+++ src/main/java/org/acme/UserResource.java
@@ -3,6 +3,7 @@ package org.acme;
 import com.lowagie.text.Document;
 import com.lowagie.text.Image;
 import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfPTable;
 import com.lowagie.text.pdf.PdfWriter;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
@@ -43,10 +44,21 @@ public class UserResource {
                     Document doc = new Document();
                     PdfWriter.getInstance(doc, output);
                     doc.open();
+                    // logo
                     Image logo = Image.getInstance(getClass().getResource("/logo.png"));
                     logo.scaleToFit(100, 100);
                     doc.add(logo);
                     doc.add(new Paragraph("User: " + user.firstName + " " + user.lastName));
+
+                    // table
+
+                    PdfPTable table = new PdfPTable(2);
+                    table.addCell("First Name");
+                    table.addCell(user.firstName);
+                    table.addCell("Last Name");
+                    table.addCell(user.lastName);
+                    doc.add(table);
+
                     doc.close();
                 }).header("Content-Disposition", "inline; filename=\"user-" + id + ".pdf\"")
                 .build();
