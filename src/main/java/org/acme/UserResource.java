866b3ec9a80bf7e9bf75fb4839a617e1fc9ad06c
Gunnar Pruefer <gunnar.pruefer@gmx.de>
2025-08-31 22:27:39 +0200

    adding streamingoutput

--- src/main/java/org/acme/UserResource.java
+++ src/main/java/org/acme/UserResource.java
@@ -1,8 +1,12 @@
 package org.acme;
 
+import com.lowagie.text.Document;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.StreamingOutput;
 
 @Path("/users")
 public class UserResource {
@@ -22,6 +26,25 @@ public class UserResource {
         byte[] pdfBytes = pdfService.generateUserPdf(user);
         return Response.ok(pdfBytes)
                 .header("Content-Disposition", "inline; filename=\"user-" + id + ".pdf\"")
+                .build();
+    }
+
+    @GET
+    @Path("/{id}/pdf-stream")
+    @Produces("application/pdf")
+    public Response getUserPdfStream(@PathParam("id") Long id) {
+        PdfUser user = PdfUser.findById(id);
+        if (user == null) {
+            throw new NotFoundException("User not found");
+        }
+
+        return Response.ok((StreamingOutput) output -> {
+                    Document doc = new Document();
+                    PdfWriter.getInstance(doc, output);
+                    doc.open();
+                    doc.add(new Paragraph("User: " + user.firstName + " " + user.lastName));
+                    doc.close();
+                }).header("Content-Disposition", "inline; filename=\"user-" + id + ".pdf\"")
                 .build();
     }
 }
